42d66c7f5e204a7899675b343eca2f284c885d04
gokulakannan <gokulakannan.r@idp.com>
2024-01-07 17:17:09 +0530

    add logic completed

--- web.py
+++ web.py
@@ -4,10 +4,20 @@ import time
 
 todos = functions.get_todos()
 
-st.title("To do App")
-st.subheader(time.strftime("%d-%m-%y %H:%M:%S"))
+
+def add_todo():
+    todo = st.session_state["new_todo"]
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+st.title("My Todo App")
+st.write(time.strftime("%d-%m-%y %H:%M:%S"))
 
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add a todo...")
+st.text_input(label="", placeholder="Add a todo...",
+              on_change=add_todo, key="new_todo")
+
+st.session_state
